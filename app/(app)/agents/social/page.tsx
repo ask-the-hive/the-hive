0e282f9709da8297f8c3588c2c0ecc9d217a144a
Colin Johnson <contact@colinrjohnson.com>
2025-10-14 09:04:58 -0500

    add more page boundaries

--- app/(app)/agents/social/page.tsx
+++ app/(app)/agents/social/page.tsx
@@ -1,15 +1,16 @@
 export const dynamic = 'force-dynamic';
 
-import React from 'react'
+import React from 'react';
+import ErrorBoundary from '@/components/error-boundary';
 import AgentPage from '../_components';
 import { socialAgent } from './_data';
 
 const SocialAgentPage: React.FC = () => {
-    return (
-        <AgentPage
-            agent={socialAgent}
-        />
-    )
-}
+  return (
+    <ErrorBoundary pageKey="agent-social">
+      <AgentPage agent={socialAgent} />
+    </ErrorBoundary>
+  );
+};
 
 export default SocialAgentPage;
--- app/(app)/agents/staking/page.tsx
+++ app/(app)/agents/staking/page.tsx
@@ -1,17 +1,18 @@
 export const dynamic = 'force-dynamic';
 
-import React from 'react'
+import React from 'react';
+import ErrorBoundary from '@/components/error-boundary';
 
 import AgentPage from '../_components';
 
 import { stakingAgent } from './_data';
 
 const StakingAgentPage: React.FC = () => {
-    return (
-        <AgentPage
-            agent={stakingAgent}
-        />
-    )
-}
+  return (
+    <ErrorBoundary pageKey="agent-staking">
+      <AgentPage agent={stakingAgent} />
+    </ErrorBoundary>
+  );
+};
 
-export default StakingAgentPage; 
+export default StakingAgentPage;
